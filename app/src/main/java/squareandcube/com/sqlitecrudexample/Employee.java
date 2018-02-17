ad6d2c09c896cd03259ef7d43422bf9428880840
rohit9075 <rohit9075@gmail.com>
2018-02-17 15:02:46 +0530

    Initial commit

---
+++ app/src/main/java/squareandcube/com/sqlitecrudexample/Employee.java
@@ -0,0 +1,40 @@
+package rohit.com.sqlitecrudexample;
+
+/**
+ * Created by VIVEK on 10/25/2017.
+ */
+
+public class Employee {
+
+    int id;
+    String name, dept, joiningDate;
+    double salary;
+
+    public Employee(int id, String name, String dept, String joiningDate, double salary) {
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+        this.joiningDate = joiningDate;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public String getJoiningDate() {
+        return joiningDate;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
